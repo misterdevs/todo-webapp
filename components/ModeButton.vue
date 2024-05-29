508cb4957f8529659e8e2cf82a9f21f7887537a7
M Ridho <ridhomuhammad.rb@gmail.com>
2024-05-29 10:57:46 +0700

    chore: refactoring

---
+++ components/ModeButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="fixed bottom-5 right-5">
+    <button
+      class="p-2 bg-white dark:bg-gray-800 rounded-full text-gray-600 shadow-lg"
+      @click="toggleTheme"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="size-6"
+      >
+        <path
+          v-if="!isDarkMode"
+          d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z"
+        />
+        <path
+          v-if="isDarkMode"
+          fill-rule="evenodd"
+          d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isDark: { type: Boolean, required: true },
+  },
+  data() {
+    return {
+      isDarkMode: this.isDarkModeMode,
+    }
+  },
+  created() {
+    if (localStorage.getItem('theme') === 'dark') {
+      this.isDarkMode = true
+      // eslint-disable-next-line nuxt/no-globals-in-created
+      document.documentElement.classList.add('dark')
+    }
+  },
+  methods: {
+    toggleTheme() {
+      this.isDarkMode = !this.isDarkMode
+      if (this.isDarkMode) {
+        document.documentElement.classList.add('dark')
+        localStorage.setItem('theme', 'dark')
+      } else {
+        document.documentElement.classList.remove('dark')
+        localStorage.setItem('theme', 'light')
+      }
+    },
+  },
+}
+</script>
